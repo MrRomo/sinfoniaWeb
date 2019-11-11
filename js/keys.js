6d54cf425d79de1ac498ae1433e92b5174913c6a
Ricardo Romo <jricardoromo21@gmail.com>
2019-11-11 15:13:42 -0500

    fixed broken links, implement auto render for team cards, responsive desing implement on whole site

---
+++ js/keys.js
@@ -0,0 +1,213 @@
+const researcher = {
+    "names": {
+        "8": "Estefany Montoya Alvarez",
+        "9": "Eduardo Gonzalez Coronado ",
+        "10": "Manuel Rios Beltran",
+        "11": "Oswaldo Andrés Peña",
+        "12": "John Alexander Garzón",
+        "13": "Sindy Paola Amaya ",
+        "14": "Edgar Camilo Camacho ",
+        "15": "Jorge Sebastián Mora Lara",
+        "16": "Lina Alejandra Plazas ",
+        "17": "Nicolas Felipe Garzon ",
+        "18": "Diego Esteban Ibañez ",
+        "19": "Ervison David Plata ",
+        "20": "Tatiana Andrea Rozo Manrique ",
+        "21": "Mario Yesid Acuña ",
+        "22": "Bryan Steven Betancur Sanchez",
+        "23": "Dilan David Solar",
+        "24": "César Daniel Garrido ",
+        "25": "Juan David Garcia ",
+        "26": "Nicolas Rocha Pacheco ",
+        "27": "Juan José García ",
+        "28": "Adelaida Zuluaga Cajiao",
+        "29": "Daniel Felipe Aponte Vargas",
+        "30": "Jose Ricardo Romo ",
+        "31": "Cristhian Carpio Polo"
+    },
+    "affiliation": {
+        "8": " Bancolombia",
+        "9": " Bancolombia",
+        "10": " Bancolombia",
+        "11": " Bancolombia",
+        "12": " Bancolombia",
+        "13": "SantoTomás University",
+        "14": "SantoTomás University",
+        "15": " AndesUniversity",
+        "16": "SantoTomás University",
+        "17": "SantoTomás University",
+        "18": "SantoTomás University",
+        "19": "Magdalena University",
+        "20": " Santo Tomas University",
+        "21": " Magdalena University",
+        "22": " Santo Tomas University",
+        "23": " Magdalena University",
+        "24": " Andes University",
+        "25": " Andes University",
+        "26": " Andes University",
+        "27": " Andes University",
+        "28": "AndesUniversity",
+        "29": " Santo Tomas University",
+        "30": " Magdalena University",
+        "31": "AndesUniversity"
+    },
+    "url": {
+        "8": "images/team/estefany_montoya.webp",
+        "9": "images/team/eduardo_gonzalez.webp",
+        "10": "images/team/manuel_rios.webp",
+        "11": "images/team/oswaldo_peña.webp",
+        "12": "images/team/jhon_garzon.webp",
+        "13": "images/team/sindy_paola.webp",
+        "14": "images/team/edgar_camacho.webp",
+        "15": "images/person.webp",
+        "16": "images/team/lina_plazas.webp",
+        "17": "images/team/nicolas_garzon.webp",
+        "18": "images/team/diego_ibañez.webp",
+        "19": "images/team/ervinson_plata.webp",
+        "20": "images/team/tatiana-rozo.webp",
+        "21": "images/team/mario_acuña.webp",
+        "22": "images/team/bryan-betancur.webp",
+        "23": "images/team/dilan_solar.webp",
+        "24": "images/team/cesar_garrido.webp",
+        "25": "images/team/juan_david.webp",
+        "26": "images/team/nicolas_rocha.webp",
+        "27": "images/team/juan_garcia.webp ",
+        "28": "images/team/adelaida_zuluaga.webp",
+        "29": "images/team/felipe-aponte.webp",
+        "30": "images/team/ricardo_romo.webp ",
+        "31": "images/team/cristhian_carpio.webp"
+    },
+    "bio": {
+        "8": "Technical Analyst of AI Center of Competences. Mechatronic                                    Engineer \u2013                                    EIA University, Medellin 2017. Excellent team leadership and specialized in defining                                    solutions                                    and research related to electronics, robotics and process automation.",
+        "9": "Technical Analyst of AI Center of Competences. Mechatronic                                    Engineer,                                    EIA University, Medellin 2017. Interested in developing solutions related with AI                                    and Apis. Knowledge                                    in programming systems such as Python, Matlab, C++, VB, NodeJS and Java.                                ",
+        "10": "Expert of AI Center of Competences. Electronic Engineer,                                    Santo Tomas University, Bogota 2018. Expert in computer science and involved in                                    works related to AI,                                    machine and deep learning. Knowledge in python, matlab and java aplications.                                ",
+        "11": "Expert of AI Center of Competences. Electronic Engineer,                                    Santo Tomas University,                                    Bogota 2017. Has been involved in national AI research groups, developer of                                    different algorithm                                    solutions related to machine learning and intelligent systems. Member of a                                    robotic football team part of the 2014, 2015, 2016 and 2017 robocup editions.                                ",
+        "12": "Expert of AI Center of Competences. Magister in Industrial                                    Automation Engineering,                                    National University, Bogota 2015. Electronic Graduate,                                    National Pedagoggic University, Bogota 2006. Technical leader interested in                                    innovation                                    projects, machine learning, and computer vision. 2 years software developer                                    experience using java, python , c++. ",
+        "13": "Professor and researcher at Santo Tomás University.                                    Current member of the Robotics Study and Research Group GED -                                    GED and leader of the line of research in Robotics.                                ",
+        "14": "Electronic Engineer (Universidad Pedagógica y Tecnológica                                    de Colombia -                                    UPTC, 2015). MSc in Electronic Engineering (Universidad Nacional de Colombia, 2017).                                    Current position as lecturer in Universidad Santo Tomás. Member of research group in                                    robotics GED.                                    In charge of the artificial vision research line.                                    Research interests: Image Processing, Artificial Vision, Embedded Systems.                                ",
+        "15": "Master's student in Electrical and Electronics Engineering                                    at                                    Universidad de los Andes. SinfonIA team member. Interested on machine learning and                                    artificial vision. Researcher                                ",
+        "16": "Student electronic engineering and member of the hotbed of                                    robotics research                                    at Santo Tomás University ",
+        "17": "Student of Electronic Engineering program of the Santo                                    Tomas                                    University. Member of STOx\u2019s and Robotic Research Student Group                                ",
+        "18": "Student Electronic engineering and member of the robotic                                    Research Student Group at Santo Tomás University                                ",
+        "19": "Student of Electronic Engineering, with a good knowledge                                    and understanding                                    in programming and a good capacity in the implementation of hardware                                ",
+        "20": "Student of electronic engineering at Santo Tomas                                        university, I am in seventh semester, Member of the Symphony Pepper Team.                                    ",
+        "21": "Electronic engineering student seventh semester. Good                                        team leadership.                                        Interested in developing solutions related to AI. Basic knowledge in matlab,                                        python, vhdl.                                    ",
+        "22": "Student electronic engineering and member of the                                        hotbed                                        of robotics research at Santo Tomás University.                                    ",
+        "23": "Electronics Engineering student at Magdalena                                        University, currently studying 8th semester.                                        Member of the Sinfon\u00cdA Pepper team and IEEE student branch.                                    ",
+        "24": "Electrical and Electronics Engineering student at                                        Universidad                                        de los Andes, currently in ninth semester. Member of the SinfonIA Pepper Team                                        and IEEE Robotics                                        Automation Society University branch (RAS Uniandes). ",
+        "25": "Electrical and Electronics Engineering student at                                        Universidad                                        de los Andes, currently in ninth semester. Member of the SinfonIA Pepper Team                                        and IEEE Robotics                                        Automation Society University branch (RAS Uniandes).                                    ",
+        "26": "Electrical and Electronics Engineer from Universidad                                        de Los Andes.                                        Electronics and Computer Master student at Universidad de Los Andes. Former                                        member of IEEE student branch.                                        Main research interest on ML hardware acceleration using RISC-V ISA.                                    ",
+        "27": "Electrical and Electronics Engineering student at                                        Universidad de los Andes.                                        Member of SinfonIA Pepper team and                                        IEEE Robotics Automation Society University branch (RAS Uniandes).                                    ",
+        "28": "Electrical and Electronics Engineering student at                                    Universidad de los Andes,                                    currently in her eighth semester. Member of the SinfonIA Pepper Team and IEEE                                    Robotics Automation Society University branch (RAS Uniandes). ",
+        "29": "Student of Electronic Engineering program of the Santo                                    Tomas University. Member of the Robotic Research Student Group                                ",
+        "30": "Electronic engineering student, with skills in digital                                    design, mathematics, hardware and web programming with languages such as python,                                    javascript, c, c++, c#, console management, non-relational databases and cloud                                    services such as Amazon Web Service . He won the first place in the AI Challenge in                                    2018.                                ",
+        "31": "Electronic Engineer with an emphasis in Telecommunications                                    and a student                                    of Systems Engineering from Universidad del Magdalena, a member of the IEEE Student                                    Branch and of the MAGMA and GIDEAM research groups. Contractor of the University of                                    Magdalena.                                "
+    }
+}
+
+const executive = {
+    "names": {
+        "0": "Carlos Saith Rodriguez",
+        "1": "Diana Arismendy Rendón",
+        "2": "Oscar Salinas Rendón",
+        "3": "Fabian Eduardo Pérez",
+        "4": "Armando Mateus",
+        "5": "Eyberth Rolando Rojas ",
+        "6": "Carlos Andrés Quintero",
+        "7": "Carolina Higuera Arias "
+    },
+    "affiliation": {
+        "0": " Bancolombia",
+        "1": " Bancolombia",
+        "2": " Bancolombia",
+        "3": "SantoTomás University",
+        "4": "SantoTomás University",
+        "5": " Magdalena University",
+        "6": "AndesUniversity",
+        "7": " Andes University"
+    },
+    "url": {
+        "0": "images/team/carlos_saith.webp",
+        "1": "images/team/diana-arismendy.webp",
+        "2": "images/team/oscar_salinas.webp",
+        "3": "images/team/fabian_eduardo.webp",
+        "4": "images/team/armando_mateus.webp",
+        "5": "images/team/eyberth_rojas.webp",
+        "6": "images/team/carlos_andres.webp",
+        "7": "images/team/carolina_higuera.webp"
+    },
+    "bio": {
+        "0": "Bancolombia AI Center of Competences Technical Leader.                                    Magister in Systems and Computing Enginnering, Andes University, Bogota 2016.                                    Electronic Engineer, Santo Tomas University, Bogota 2010. Academic leader in                                    research teams related to AI during 8 years with                                    participation in multiple international events and publications of the topic.",
+        "1": "Bancolombia AI Center of Competences Functional Leader.                                    MBA, EADA Business School, Barcelona 2014. International Business, EAFIT University,                                    Medellin 2007.                                    Professional with senior experience in digital business strategy and evolution and                                    project management.                                ",
+        "2": "AI Business Analyst - AI Center of Competences.                                    MBA, EAFIT University, Medellin 2013, Industrial Engineer, EIA University,                                    Medellin 2011. Professional with 8 years of experience in process and service                                    design.                                    Customer relationship and innovation knowledge.",
+        "3": "Professor in Electronic Engineering program of the                                    Universidad Santo Tomás and Leader of Investigation Grup GED (Study and Development                                    in Robotic).                                    Professional with experience in desing of SCADA, Automation and Communications                                    systems for oil and gas industries.                                ",
+        "4": "Professor and researcher in Robotics in Electronics at the                                    Universidad Santo Tomás.                                    Current member of the Robotics Study and Research Group GED and director                                    of the Junior Research Group in Robotics at the Universidad Santo Tomás.",
+        "5": "Professor and Director of Robotics area in Electronic                                    Engineering program                                    of the Universidad del Magdalena. Co-founder and active member between the years                                    2011 and 2017 of STOx\u2019s,                                    team that has participated in RoboCup SSL since 2011.                                ",
+        "6": "M.Sc on Electronic and Computer Engineering at Universidad                                    de los Andes                                    (2011) and Electronic Engineer at the same university (2009). Current SinfonIA                                    Pepper Team Leader.                                    Researcher and Instructor at Universidad Santo Tomás (2013-2017) and Universidad de                                    los Andes (2018)                                ",
+        "7": "M.Sc on Electronic and Computer Engineering at Universidad                                    de los Andes (2017) and Electronic Engineer at Universidad Pedagógica y tecnológica                                    de Colombia (2015).                                    Researcher and Instructor at Universidad Santo Tomás (2018-2019) and Universidad de                                    los Andes (2019).                                    Researcher and SinfonIA team leader.                                "
+    }
+}
+
+
+const object2List = (obj) => {
+    const affiliation = Object.values(obj.affiliation)
+    const bio = Object.values(obj.bio)
+    const url = Object.values(obj.url)
+    const names = Object.values(obj.names)
+    const objList = []
+    for (let i = 0; i < affiliation.length; i++) {
+        objList.push({
+            name: names[i],
+            bio: bio[i],
+            url: url[i],
+            affiliation: affiliation[i]
+        })
+    }
+    return objList
+}
+
+console.log()
+
+
+const personTemplate = (content) => {
+    const htmlString = `<div class="card col-md-3">
+                            <div class="card-header">
+                                <img class="img-fluid" src="${content.url}"
+                                    alt="${content.url}">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">
+                                    ${content.name}
+                                </h5>
+                                <h6 class="blog-title card-title font-weight-bold"><strong>Affiliation: </strong> ${content.affiliation}</h6>
+                                <p style="text-align:justify">${content.bio}</p>
+                            </div>
+                        </div>`
+    return htmlString
+}
+function createTemplate(HTMLString) {
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString;
+    return html.body.children[0];
+}
+
+function renderContents($container, list,template) {
+    console.log(list)
+    list.forEach((content) => {
+      const HTMLString = template(content);
+      const contentElement = createTemplate(HTMLString);
+      $container.append(contentElement);
+      const image = contentElement.querySelector('img');
+      image.addEventListener('load', (event) => {
+        event.srcElement.classList.add('fadeIn');
+      })
+    })
+    console.log("Generated...");
+}
+
+
+const $researcher = document.querySelector('.researcher');
+const $executive = document.querySelector('.executive');
+
+renderContents($researcher, object2List(researcher), personTemplate)
+renderContents($executive, object2List(executive), personTemplate)
